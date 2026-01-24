e44a0b50059b8636fb4775266a042fda8d44c872
John Anthony Pesco <pescojohnanthony@gmail.com>
2026-01-25 01:52:27 +0800

    add new modal in current stock page: add stock modal

--- resources/views/admin/inventory/currentstock.blade.php
+++ resources/views/admin/inventory/currentstock.blade.php
@@ -44,7 +44,7 @@
         <div class="flex flex-col lg:flex-row justify-between items-center mt-10">
             <h1 class="text-3xl font-bold text-[#EB7100]">Fertilizers</h1>
             <div class="flex gap-2 items-center">
-                <button class="w-fit h-fit bg-[#046636] p-1 rounded-lg px-5 text-white font-bold">
+                <button id="addstockbtn" class="w-fit h-fit bg-[#046636] p-1 rounded-lg px-5 text-white font-bold">
                     Add Stock
                 </button>
                 <button class="w-fit h-fit bg-[#046636]/20 border-2 border-[#046636] p-1 rounded-lg px-5 text-[#046636] font-bold">
@@ -157,14 +157,115 @@
                             </div>
                         </td>
                     </tr>
-
-                    <!-- Add more rows here as needed -->
-
                 </tbody>
             </table>
         </div>
+
+        {{-- add stock modal --}}
+
+        <x-admin.modal modalid="addstockmodal" modaltitle="Add Stock" closemodal="closeaddstockmodal">
+            <form action="" class="mt-5 max-h-[80vh] overflow-auto px-2">
+                <div class="relative">
+                    <select name="products" id="products" class="border-2 border-[#046636] rounded-lg w-full px-4 text-[#046636] font-bold bg-[#046636]/20 text-center p-2">
+                        <option value="">Choose a supplier</option>
+                        <option value="">Mang Karding</option>
+                    </select>
+                    <span class="absolute top-1 right-5 text-red-700 font-bold text-xl">*</span>
+                </div>
+                <div class="flex gap-2 mt-3">
+                    <div class="w-1/2">
+                        <label for="ref" class="text-[#046636] font-bold">Ref No.</label>
+                        <input type="text" name="ref" id="ref" class="border-2 border-[#046636] placeholder:font-semibold placeholder:text-[#046636]/50 rounded-lg w-full px-4 text-[#046636] p-2" placeholder="e.g 123456">
+                    </div>
+                    <div class="w-1/2">
+                        <label for="ref" class="text-[#046636] font-bold">Date:</label>
+                        <input type="date" name="date" id="date" class="border-2 border-[#046636] placeholder:font-semibold placeholder:text-[#046636]/50 rounded-lg w-full px-4 text-[#046636] p-2" placeholder="e.g 123456">
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <select name="products" id="products" class="border-2 border-[#046636] rounded-lg w-full px-4 text-[#046636] font-bold bg-[#046636]/20 text-center p-2">
+                        <option value="">Choose a supplier</option>
+                        <option value="">Mang Karding</option>
+                    </select>
+                    <span class="absolute top-1 right-5 text-red-700 font-bold text-xl">*</span>
+                </div>
+
+                <div class="mt-5 border-t-2 border-blue-700">
+                    <h1 class="text-blue-700 font-bold text-xl mt-3">Container 1:</h1>
+
+                    <div class=" mt-3">
+                        <label for="boxesqty" class="text-[#046636] font-bold">Quantity:</label>
+                        <div class="flex gap-2">
+                            <div class="relative w-1/2">
+                                <input type="boxesqty" name="date" id="date" class="border-2 border-[#046636] placeholder:font-semibold placeholder:text-[#046636]/50 rounded-lg w-full px-4 text-[#046636] p-2.5" placeholder="e.g 123">
+                                <span class="absolute top-2 right-5 text-red-700 font-bold text-xl">*</span>
+                            </div>
+                            <h1 class="w-1/2 text-[#046636] font-bold text-lg p-2 bg-[#046636]/20 rounded-lg border-2 border-[#046636]">Type:Boxes</h1>
+                        </div>
+                        <label for="uomqty" class="text-[#046636] font-bold">Quantity:</label>
+                        <div class="flex gap-2 mt-2">
+                            <div class="relative w-1/2">
+                                <input type="uomqty" name="date" id="date" class="border-2 border-[#046636] placeholder:font-semibold placeholder:text-[#046636]/50 rounded-lg w-full px-4 text-[#046636] p-2.5" placeholder="e.g 123">
+                                <span class="absolute top-2 right-5 text-red-700 font-bold text-xl">*</span>
+                            </div>
+                            <h1 class="w-1/2 text-[#046636] font-bold text-lg p-2 bg-[#046636]/20 rounded-lg border-2 border-[#046636]">UoM:Boxes</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-5 border-t-2 border-blue-700">
+                    <h1 class="text-blue-700 font-bold text-xl mt-3">Container 1:</h1>
+                    <h1 class="text-[#046636] font-bold text-lg mt-3 p-2 bg-[#046636]/20 rounded-lg border-2 border-[#046636]">Inside:Boxes</h1>
+
+                    <div class=" mt-3">
+                        <label for="ref" class="text-[#046636] font-bold">Quantity:</label>
+                        <div class="flex gap-2">
+                            <div class="relative w-1/2">
+                                <input type="text" name="date" id="date" class="border-2 border-[#046636] placeholder:font-semibold placeholder:text-[#046636]/50 rounded-lg w-full px-4 text-[#046636] p-2.5" placeholder="e.g 123">
+                                <span class="absolute top-2 right-5 text-red-700 font-bold text-xl">*</span>
+                            </div>
+                            <h1 class="w-1/2 text-[#046636] font-bold text-lg p-2 bg-[#046636]/20 rounded-lg border-2 border-[#046636]">Type:Boxes</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-5 border-t-2 border-blue-700">
+                    <div class=" mt-3">
+                        <label for="expirydate" class="text-[#046636] font-bold">Expirty Date:</label>
+                        <div class="flex gap-2">
+                            <div class="relative w-1/2">
+                                <input type="date" name="expirydate" id="expirydate" class="border-2 border-[#046636] placeholder:font-semibold placeholder:text-[#046636]/50 rounded-lg w-full px-4 text-[#046636] p-2.5" placeholder="e.g 123">
+                            </div>
+                            <button class="w-1/2 text-[#046636] font-bold text-lg p-2 rounded-lg border-2 border-[#046636]"><i class="fa-regular fa-print text-[#046636]"></i> Print Copy</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-2 mt-5">
+                    <button class="text-[#046636] font-bold text-lg p-2 rounded-lg border-2 border-[#046636]">Create and Add Stock Again</button>
+                    <button class="text-white font-bold text-lg p-2 rounded-lg border-2 bg-[#046636]">Create</button>
+                </div>
+            </form>
+        </x-admin.modal>
     </main>
 
 </body>
 <script src="{{ asset('js/admin/navbar.js') }}"></script>
+<script>
+    function addStockModal() {
+        const modal = document.getElementById('addstockmodal');
+        const openModalButton = document.getElementById('addstockbtn');
+        const closeModalButton = document.getElementById('closeaddstockmodal');
+
+        openModalButton.addEventListener('click', () => {
+            modal.classList.remove('hidden');
+        });
+
+        closeModalButton.addEventListener('click', () => {
+            modal.classList.add('hidden');
+        });
+    }
+    
+    addStockModal();
+</script>
 </html>
--- resources/views/components/admin/modal.blade.php
+++ resources/views/components/admin/modal.blade.php
@@ -1,6 +1,6 @@
 @props(['modalid' => '', 'modaltitle' => '', 'closemodal' => '', 'modalwidth' => 'max-w-lg'])
 
-<div class="bg-black/50 backdrop-blur-[3px] w-full h-full fixed top-0 left-0 flex items-center justify-center px-5 hidden z-2" id="{{$modalid}}">
+<div class="bg-black/50 backdrop-blur-[3px] w-full h-full fixed top-0 left-0 flex items-center justify-center px-5 z-2 hidden" id="{{$modalid}}">
     <div class="modal bg-white w-full p-5 rounded-2xl {{$modalwidth}}">
         <div class="flex justify-between items-center sticky top-0 bg-white">
             <h1 class="text-3xl text-[#046636] font-bold">{{$modaltitle}}</h1>
