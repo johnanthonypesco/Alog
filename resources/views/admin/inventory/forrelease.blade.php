b0f58d8d865b6a649baa8da5f90494accbf82b2f
John Anthony Pesco <pescojohnanthony@gmail.com>
2026-01-27 10:12:57 +0800

    inventory has now a complete page

--- resources/views/admin/inventory/forrelease.blade.php
+++ resources/views/admin/inventory/forrelease.blade.php
@@ -133,6 +133,96 @@
                 </tbody>
             </table>
         </div>
+        
+        <h1 class="text-blue-700/70 text-3xl font-bold mt-10">Storage 1</h1>
+        <div class="flex flex-col lg:flex-row justify-between items-center">
+            <h1 class="text-3xl font-bold text-[#EB7100]">Chemichals</h1>
+            <div id="addarchivebtn" class="flex gap-2 items-center">
+                <button id="addstockbtn" class="w-fit h-fit p-2 bg-[#046636] rounded-lg px-5 text-white font-bold">
+                    Create Releases
+                </button>
+                <button id="archivedstockbtn" class="w-fit h-fit p-2 bg-[#046636]/20 border-2 border-[#046636] rounded-lg px-5 text-[#046636] font-bold">
+                    Archived Releases
+                </button>
+            </div>
+            <h1 id="discrepancytitle" class="text-xl font-bold text-red-600 border-2 p-2 rounded-lg border-red-700 hidden">Discrepancy Detected : 1</h1>
+        </div>
+
+        <div class="w-full overflow-x-auto border-2 border-[#046636] rounded-xl mt-4">
+            <table class="w-full min-w-[1100px] text-[#046636]">
+                <thead class="bg-[#046636]/10 text-[#046636] font-semibold">
+                    <tr>
+                        <th class="p-5 text-left">
+                            <div class="flex items-center gap-2">
+                                Product
+                                <button type="button">
+                                    <i class="fa-solid fa-angle-down text-sm text-[#EB7100]"></i>
+                                </button>
+                            </div>
+                        </th>
+
+                        <th class="p-5 text-center">
+                            <div class="flex flex-col items-center leading-tight">
+                                <div class="border-b-2 border-[#EB7100] pb-0.5 mb-1 w-fit">Type</div>
+                                <div class="text-[#EB7100] text-sm font-medium">On Bag</div>
+                            </div>
+                        </th>
+
+                        <th class="p-5 text-center">
+                            <div class="flex flex-col items-center leading-tight">
+                                <div class="border-b-2 border-[#EB7100] pb-0.5 mb-1 w-fit">UoM</div>
+                                <div class="text-[#EB7100] text-sm font-medium">On Hand</div>
+                            </div>
+                        </th>
+                        <th>Taken From</th>
+                        <th>Status</th>
+                        <th>Customer</th>
+
+                        <th class="p-5 text-center">Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200 text-gray-700">
+                    <tr class="hover:bg-[#046636]/5 transition-colors">
+                        <td class="p-5 font-medium flex flex-col">
+                            <span>14-14-14</span>
+                            <span class="font-bold text-[#046636]">Swire</span>
+                        </td>
+
+                        <td class="p-5 text-center">
+                            <div class="flex flex-col items-center">
+                                <div>₱138,372.12</div>
+                                <div class="font-bold text-[#EB7100]">50</div>
+                            </div>
+                        </td>
+
+                        <td class="p-5 text-center">
+                            <div class="flex flex-col items-center">
+                                <div>Bag</div>
+                                <div class="font-bold text-[#EB7100]">78</div>
+                            </div>
+                        </td>
+                        <td class="text-center">on hand</td>
+                        <td class="text-center">Pending</td>
+                        <td class="text-center">John Doe</td>
+
+                        <td id="actionbtn" class="p-5 text-center">
+                            <div class="flex items-center justify-center gap-3">
+                                <button id="innercontainerbtn" class="p-2 bg-green-100 hover:bg-green-200 rounded-md transition">
+                                    <i class="fa-solid fa-folders text-green-700"></i>
+                                </button>
+                                <button class="p-2 bg-blue-100 hover:bg-blue-200 rounded-md transition">
+                                    <i class="fa-solid fa-pencil text-blue-700"></i>
+                                </button>
+                                <button class="p-2 bg-red-100 hover:bg-red-200 rounded-md transition">
+                                    <i class="fa-solid fa-trash-can text-red-700"></i>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
         <x-admin.modal modalid="innercontainermdoal" modalwidth="max-w-2xl" modaltitle="Inner Container" closemodal="closeinnercontainermodal">
             <h1 class="text-[#EB7100] font-semibold text-xl md:text-2xl">From: 14-14-14 Swire</h1>
